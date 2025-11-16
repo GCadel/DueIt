14a3b7027ce7a9f7df9c5ebaaafe32de4b526985
justin <jmdingeman@gmail.com>
2025-11-16 11:15:05 -0800

    add categories controller and router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv'
 import UserRouter from "./routes/users.js";
+import CategoriesRouter from "./routes/categories.js"
 
 dotenv.config()
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/user", UserRouter);
+app.use("/categories", CategoriesRouter)
 
 const PORT = process.env.PORT || 3000;
 
