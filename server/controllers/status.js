fd13f309d7477838e282f4431ea487b3c0a5adac
justin <jmdingeman@gmail.com>
2025-11-17 15:32:05 -0800

    Small corrections to status.js including so ids match to router.

--- server/controllers/status.js
+++ server/controllers/status.js
@@ -14,28 +14,28 @@ const getStatus = async (req, res) => {
 };
 
 const getStatusById = async (req, res) => {
-  const statusId = req.params.statusId;
+  const statusId = req.params.id;
   try {
     const selectQuery = `
      SELECT *
     FROM statuses
     WHERE id=$1`;
     const results = await pool.query(selectQuery, [statusId]);
-    res.status(200).json(results.rows);
+    res.status(200).json(results.rows[0]);
   } catch (err) {
     res.status(409).json({ error: err.message });
   }
 };
 
 const deleteStatusById = async (req, res) => {
-  const statusId = req.body.id;
+  const statusId = req.params.id;
   try {
     const selectQuery = `
     DELETE
     FROM statuses
     WHERE id=$1`;
     const results = await pool.query(selectQuery, [statusId]);
-    res.status(200).json(results.rows);
+    res.status(200).json(results.rows[0]);
   } catch (err) {
     res.status(409).json({ error: err.message });
   }
@@ -50,24 +50,25 @@ const createStatus = async (req, res) => {
     const values = [data.name];
 
     const result = await pool.query(insertQuery, values);
-    res.status(200).json(result.rows);
+    res.status(200).json(result.rows[0]);
   } catch (err) {
     res.status(409).json({ error: err.message });
   }
 };
 
 const updateStatus = async (req, res) => {
+  const statusId = req.params.id
   const data = req.body;
   try {
     const updateQuery = `
     UPDATE statuses
     SET 
-      name=$1, 
+      name=$1 
     WHERE id=$2`;
-    const values = [data.name, data.id];
+    const values = [data.name, statusId];
 
     const result = await pool.query(updateQuery, values);
-    res.status(200).json(result.rows);
+    res.status(200).json(result.rows[0]);
   } catch (err) {
     res.status(409).json({ error: err.message });
   }
