8049cdc42eb73a1d30d7d2d9c49c99dfb9b22b8b
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-11-20 02:30:53 +0900

    Prepare board for drag and drop module

--- client/src/features/TaskCard.jsx
+++ client/src/features/TaskCard.jsx
@@ -1,47 +1,40 @@
 export const TaskCard = ({ taskData, user }) => {
-  if (taskData) {
-    return (
-      <div className='task-card'>
-        <h3>{taskData.name ? taskData.name : "Example Task"}</h3>
-        <div className='justify-apart'>
-          <div className='user-chip'>
-            {user !== "Unassigned" ? (
-              <div className=' justify-apart'>
-                <div className='avatar'>
-                  {user.charAt(0).toUpperCase()}
-                </div>
-                <p>{user}</p>
-              </div>
-            ) : (
-              <span style={{ color: "red" }}>Unassigned</span>
-            )}
-          </div>
-          <div className='category-chip'>
-            {taskData.category ? taskData.category : ""}
-          </div>
-        </div>
-        <div className='task-update-date'>
-          <em>
-            Last update:{" "}
-            {taskData.last_update
-              ? taskData.last_update
-              : new Date().toDateString()}
-          </em>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <CardContent
+        taskData={taskData}
+        user={user}
+      />
+    </div>
+  );
+};
+
+const CardContent = ({ taskData, user }) => {
   return (
     <div className='task-card'>
-      <h3>Example Task</h3>
+      <h3>{taskData.name ? taskData.name : "Example Task"}</h3>
       <div className='justify-apart'>
         <div className='user-chip'>
-          <span style={{ color: "red" }}>Unassigned</span>
+          {user !== "Unassigned" ? (
+            <div className=' justify-apart'>
+              <div className='avatar'>{user.charAt(0).toUpperCase()}</div>
+              <p>{user}</p>
+            </div>
+          ) : (
+            <span style={{ color: "red" }}>Unassigned</span>
+          )}
         </div>
-        <div className='category-chip'>chip example</div>
+        <div className='category-chip'>
+          {taskData.category ? taskData.category : ""}
+        </div>
       </div>
       <div className='task-update-date'>
-        <em>Last update: {new Date().toDateString()}</em>
+        <em>
+          Last update:{" "}
+          {taskData.last_update
+            ? taskData.last_update
+            : new Date().toDateString()}
+        </em>
       </div>
     </div>
   );
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,60 +1,60 @@
 import { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import { TaskCard } from "../features/TaskCard";
+import { Link } from "react-router-dom";
+
 import { TaskList } from "../features/TaskList";
-import PlusImage from "../assets/plus.png"
+import PlusImage from "../assets/plus.png";
 import TasksAPI from "../services/TasksAPI";
 import UsersAPI from "../services/UsersAPI";
 
 export const TasksPage = () => {
-
   const [tasks, setTasks] = useState([]);
   const [users, setUsers] = useState({});
 
   useEffect(() => {
     const fetchTasks = async () => {
       const data = await TasksAPI.getAllTasks();
-      setTasks(data)
-    }
+      setTasks(data);
+    };
 
     const fetchUsers = async () => {
       const userArray = await UsersAPI.getAllUsers();
       const userMap = {};
-      userArray.forEach(u => {
-        userMap[u.id] = u.first_name + ' ' + u.last_name;
+      userArray.forEach((u) => {
+        userMap[u.id] = u.first_name + " " + u.last_name;
       });
 
       setUsers(userMap);
-    }
+    };
 
     fetchTasks();
     fetchUsers();
-  }, [])
+  }, []);
 
-  const backlogTasks = tasks.filter(task => task.status_id === 1)
-  const inProgressTasks = tasks.filter(task => task.status_id === 2)
-  const reviewTasks = tasks.filter(task => task.status_id === 3)
-  const completeTasks = tasks.filter(task => task.status_id === 4)
+  const backlogTasks = tasks.filter((task) => task.status_id === 1);
+  const inProgressTasks = tasks.filter((task) => task.status_id === 2);
+  const reviewTasks = tasks.filter((task) => task.status_id === 3);
+  const completeTasks = tasks.filter((task) => task.status_id === 4);
 
   return (
     <>
       <h1>Tasks</h1>
+
       <div className='task-categories-grid'>
         <TaskList
           title={"Backlog"}
           data={backlogTasks}
           users={users}
         />
-        <TaskList 
-          title={"In Progress"} 
+        <TaskList
+          title={"In Progress"}
           data={inProgressTasks}
           users={users}
         />
-        <TaskList 
+        <TaskList
           title={"Review"}
           data={reviewTasks}
           users={users}
-          />
+        />
 
         <TaskList
           title={"Complete"}
@@ -62,8 +62,14 @@ export const TasksPage = () => {
           users={users}
         />
       </div>
-      <div className="add-task-button">
-        <Link to="/create-task"><img src={PlusImage} alt="plus sign"></img></Link>
+
+      <div className='add-task-button'>
+        <Link to='/create-task'>
+          <img
+            src={PlusImage}
+            alt='plus sign'
+          ></img>
+        </Link>
       </div>
     </>
   );
